4b3afe09fff58bddd70bf42e7b21196888d379e4
Sssubut <pomnim.skorbom@mail.ru>
2024-05-05 22:55:02 +0300

    CRUD ДЛЯ ТЕГОВ

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,17 @@ use App\Http\Controllers\Admin\Category\ShowController;
 use App\Http\Controllers\Admin\Category\StoreController;
 use App\Http\Controllers\Admin\Category\UpdateController;
 use App\Http\Controllers\Admin\Category\DeleteController;
+
+use App\Http\Controllers\Admin\Tag\TagController;
+use App\Http\Controllers\Admin\Tag\CreateTagController;
+use App\Http\Controllers\Admin\Tag\EditTagController;
+use App\Http\Controllers\Admin\Tag\ShowTagController;
+use App\Http\Controllers\Admin\Tag\StoreTagController;
+use App\Http\Controllers\Admin\Tag\UpdateTagController;
+use App\Http\Controllers\Admin\Tag\DeleteTagController;
+
 use App\Http\Controllers\Admin\Main\AdminController;
+
 use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,16 +36,8 @@ Route::group(['namespace' => '\App\Http\Controllers\Main'], function (){
     Route::get('/', IndexController::class);
 });
 
-//Route::group(['namespace' => '\App\Http\Controllers\Admin' ] , function (){
-//    Route::group(['namespace' => 'Main', 'prefix' => 'admin'], function (){
-//            Route::get('/', AdminController::class);
-//        Route::group(['namespace' => 'Category' , 'prefix' => 'categories'], function (){
-//            Route::get('/' , CategoryController::class);
-//        });
-//    });
-//
-//});
 
+//Для категорий
 Route::group(['prefix' => 'admin'], function (){
     Route::get('/', AdminController::class)->name('admin.index');
     Route::get('/category' , CategoryController::class)->name('admin.categories.index');
@@ -47,16 +49,18 @@ Route::group(['prefix' => 'admin'], function (){
     Route::delete('/{category}' , DeleteController::class)->name('admin.categories.delete');
 });
 
+//Для тегов
+Route::group(['prefix' => 'tags'], function (){
+    Route::get('/tag' , TagController::class)->name('admin.tag.index');
+    Route::get('/create' , CreateTagController::class)->name('admin.tag.create');
+    Route::post('/' , StoreTagController::class)->name('admin.tag.store');
+    Route::get('/{tag}' , ShowTagController::class)->name('admin.tag.show');
+    Route::get('/{tag}/edit' , EditTagController::class)->name('admin.tag.edit');
+    Route::patch('/{tag}' , UpdateTagController::class)->name('admin.tag.update');
+    Route::delete('/{tag}' , DeleteTagController::class)->name('admin.tag.delete');
+});
+
 
 Auth::routes();
 
 
-
-
-//Route::group(['namespace' => '\App\Http\Controllers\Admin\Main', 'prefix' => 'admin'], function (){
-//        Route::get('/', AdminController::class);
-//    Route::group(['namespace' => 'App\Http\Controllers\Admin\Category' , 'prefix' => 'categories'], function (){
-//        Route::get('/' , CategoryController::class);
-//    });
-//
-//});
